461dd54a7c265b49e31f3ca5223f4e6c7fb5b96c
ddokupil <dariusz.dokupil@intel.com>
2018-05-10 13:55:26 +0200

    minor fixes

--- .ci/jenkins/runCI.sh
+++ .ci/jenkins/runCI.sh
@@ -43,10 +43,15 @@ create_base_img()
 
 create_test_img()
 {
-    echo -----------------------Build Test image-----------------------------------
-    BASE_IMAGE=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -m 1 base_ngraph-onnx)
-    docker build --build-arg BASE_IMAGE=$BASE_IMAGE -t test_ngraph-onnx -f .ci/jenkins/test_ngraph-onnx.dockerfile .
-    return $?
+    if docker images --format "{{.Repository}}:{{.Tag}}" | grep -m 1 test_ngraph-onnx; then
+        echo Test image found so no need to build it again
+        return $?
+    else
+        echo -----------------------Build Test image-----------------------------------
+        BASE_IMAGE=$(docker images --format "{{.Repository}}:{{.Tag}}" | grep -m 1 base_ngraph-onnx)
+        docker build --build-arg BASE_IMAGE=$BASE_IMAGE -t test_ngraph-onnx -f .ci/jenkins/test_ngraph-onnx.dockerfile .
+        return $?
+    fi
 }
 
 run_test_img()
@@ -76,11 +81,11 @@ if [ "$#" -eq 0 ]; then
         echo Testing returned success, do you want me to remove the test image?
         read decission
         if [ $decission -eq "y" ] || [ $decission -eq "yes" ]; then
-            remove_test_image()
+            remove_test_image
             echo Would you like to remove base image as well?
             read decission
             if [ $decission -eq "y" ] || [ $decission -eq "yes" ]; then
-                remove_base_image()
+                remove_base_image
             fi
         fi
     fi
