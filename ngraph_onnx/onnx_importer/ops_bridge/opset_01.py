99b3111b19cb582035d1d5c077f1cca8a77a3c30
Adam Rogowiec <adam.rogowiec@intel.com>
2018-06-26 19:14:47 +0200

    GRU-1 and Reshape-5 with NotImplementedError.

--- ngraph_onnx/onnx_importer/ops_bridge/opset_01.py
+++ ngraph_onnx/onnx_importer/ops_bridge/opset_01.py
@@ -869,3 +869,9 @@ def Size(onnx_node, ng_inputs):  # type: (NodeWrapper, List[NgraphNode]) -> Ngra
     """Return input size."""
     # Dtype int64 is required for ONNX unit tests.
     return ng.constant(flatten(ng_inputs[0], 0).shape[1], dtype=np.int64)
+
+
+@refactoring_required
+def GRU(onnx_node, ng_inputs):  # type: (NodeWrapper, List[NgraphNode]) -> NgraphNode
+    """Return node performing GRU operation."""
+    return None
