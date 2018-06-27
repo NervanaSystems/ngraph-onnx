Fix broadcast issues in numpy broadcasting

ngraph_onnx/onnx_importer/ops_bridge/opset_01.py
@@ -188,11 +188,11 @@ def PRelu(onnx_node, ng_inputs):  # type: (NodeWrapper, List[NgraphNode]) -> Ngr
     if len(slope.shape) == 0:
         return ng.maximum(slope * x, x)
     elif slope.shape[0] == 1:
-        slope = ng.broadcast(slope, [x.shape[0], 1])
+        slope = ng.broadcast_to(slope, [x.shape[0], 1])
         slope = ng.reshape(slope, [x.shape[0]])
-        return ng.maximum(ng.broadcast(slope, x.shape, 0) * x, x)
+        return ng.maximum(ng.broadcast_to(slope, x.shape, 0) * x, x)
     else:
-        return ng.maximum(ng.broadcast(slope, x.shape, 1) * x, x)
+        return ng.maximum(ng.broadcast_to(slope, x.shape, 1) * x, x)
 
 
 def ThresholdedRelu(onnx_node, ng_inputs):  # type: (NodeWrapper, List[NgraphNode]) -> NgraphNode
@@ -387,8 +387,8 @@ def ReduceMean(onnx_node, ng_inputs):  # type: (NodeWrapper, List[NgraphNode]) -
     sum_node = make_reduction_op(ng.sum, onnx_node, ng_inputs[0])
     reduction_axes = get_reduction_axes(onnx_node, ng_inputs[0])
     avg_elem_count = np.prod([input_shape[x] for x in reduction_axes])
-    const_node = ng.broadcast(ng.constant(avg_elem_count, get_dtype(sum_node.get_element_type())),
-                              sum_node.shape)
+    const_node = ng.broadcast_to(ng.constant(avg_elem_count, get_dtype(sum_node.get_element_type())),
+                                 sum_node.shape)
     return ng.divide(sum_node, const_node)
 
 

tests/test_ops_binary.py
@@ -94,25 +94,12 @@ def test_add():
     ((5, 4), (1,)),
     ((5, 4), (4,)),
     ((15, 3, 5), (3, 5)),
-])
-def test_add_opset7(left_shape, right_shape):
-    """Test Add-7 operator, which uses numpy-style broadcasting."""
-    left_input = np.ones(left_shape)
-    right_input = np.ones(right_shape)
-    assert np.array_equal(import_and_compute('Add', left_input, right_input, opset=7),
-                          left_input + right_input)
-
-
-# -> NC5-191
-@pytest.mark.xfail(reason='Need to support numpy-style broadcasting')
-@pytest.mark.parametrize('left_shape,right_shape', [
     ((15, 3, 5), (15, 1, 5)),
     ((15, 3, 5), (3, 1)),
     ((8, 1, 6, 1), (7, 1, 5)),
 ])
-def test_add_opset7_failing(left_shape, right_shape):
+def test_add_opset7(left_shape, right_shape):
     """Test Add-7 operator, which uses numpy-style broadcasting."""
-    # TODO: fix broadcasting problem and merge into test above
     left_input = np.ones(left_shape)
     right_input = np.ones(right_shape)
     assert np.array_equal(import_and_compute('Add', left_input, right_input, opset=7),