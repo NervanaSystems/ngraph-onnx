24ab49ae913e86dd15059e5eb185dcc4f6ae4c45
Chruscinski, Michal <michal.chruscinski@intel.com>
2018-10-09 18:35:55 +0200

    Don't remove docker image during cleanup

--- .ci/jenkins/runCI.sh
+++ .ci/jenkins/runCI.sh
@@ -57,7 +57,6 @@ function cleanup() {
     docker exec "${DOCKER_CONTAINER}" bash -c "rm -rf /root/ngraph /root/ngraph_dist /root/.tox /root/.onnx /root/__pycache__ /root/ngraph_onnx.egg-info /root/cpu_codegen"
     docker exec "${DOCKER_CONTAINER}" bash -c 'rm -rf $(find /root/ -user root)'
     docker rm -f "${DOCKER_CONTAINER}"
-    docker rmi --force ngraph-onnx:ubuntu-16_04
 }
 
 PATTERN='[-a-zA-Z0-9_]*='
