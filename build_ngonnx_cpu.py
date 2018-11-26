6829144a1cd63e28fbe268346c7307f4955b330c
arogowie-intel <adam.rogowiec@intel.com>
2018-11-26 10:44:00 +0100

    Recipe for building nGraph-ONNX wheel and testing it.
    Signed-off-by: arogowie-intel <adam.rogowiec@intel.com>

--- build_ngonnx_cpu.py
+++ build_ngonnx_cpu.py
@@ -14,12 +14,9 @@
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
 # ==============================================================================
-import argparse
-import errno
 import multiprocessing
 import os
 from subprocess import check_call
-import sys
 import glob
 
 def clone_repo(target_name, repo, *args, version='master'):
@@ -64,11 +61,7 @@ def build_ngraph(src_location, cmake_flags):
 
     # mkdir build directory
     path = 'build'
-    try:
-        os.makedirs(path)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
+    os.makedirs(path, exist_ok=True)
 
     # Run cmake
     os.chdir('build')
@@ -131,33 +124,100 @@ def install_virtual_env(venv_dir):
 
 def load_venv(venv_dir):
     activate_this_file = os.path.abspath(venv_dir) + "/bin/activate_this.py"
-    # The execfile API is for Python 2. We keep here just in case you are on an
-    # obscure system without Python 3
-    # execfile(activate_this_file, dict(__file__=activate_this_file))
     exec(
         compile(
             open(activate_this_file, "rb").read(), activate_this_file, 'exec'),
         dict(__file__=activate_this_file), dict(__file__=activate_this_file))
 
+def install_wheel(wheel_path):
+    check_call(['pip', 'install', '-U', wheel_path])
+
 def main():
-    '''
-    Builds TensorFlow, ngraph, and ngraph-tf for python 3
-    '''
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument(
-        '--debug_build',
-        help="Builds a debug version of the nGraph components\n",
-        action="store_true")
-
-    arguments = parser.parse_args()
-
-    if (arguments.debug_build):
-        print("Building in DEBUG mode\n")
+    """
+    Builds nGraph, and nGraph-ONNX for python 3
+    """
 
     #-------------------------------
     # Recipe
     #-------------------------------
+
+    # Create the build directory
+    build_dir = os.path.abspath('build')
+    os.makedirs(build_dir, exist_ok=True)
+    print('Build location: ' + os.path.abspath(build_dir))
+
+    pwd = os.getcwd()
+    os.chdir(build_dir)
+
+    # Component versions
+    ngraph_version = "master"
+
+    # Download nGraph
+    clone_repo(
+        "ngraph",
+        "https://github.com/NervanaSystems/ngraph.git",
+        "--single-branch",
+        version=ngraph_version)
+
+    # Now build nGraph
+    ngraph_install_dir = os.path.join(build_dir, 'ngraph_dist')
+    ngraph_cmake_flags = [
+        '-DCMAKE_BUILD_TYPE=RELEASE',
+        '-DCMAKE_INSTALL_PREFIX=' + ngraph_install_dir,
+        '-DNGRAPH_TOOLS_ENABLE=YES',
+        '-DNGRAPH_USE_PREBUILT_LLVM=TRUE',
+        '-DNGRAPH_ONNX_IMPORT_ENABLE=TRUE',
+        '-DNGRAPH_UNIT_TEST_ENABLE=FALSE',
+        '-DNGRAPH_PYTHON_BUILD_ENABLE=ON',
+    ]
+
+    print('----- Build nGraph -----')
+    build_ngraph('./ngraph', ngraph_cmake_flags)
+    pyngraph_whl = build_pyngraph('./ngraph', ngraph_install_dir)
+    print('SUCCESS! Generated wheel: {}'.format(pyngraph_whl))
+
+    print('----- Build nGraph-ONNX -----')
+    ngonnx_whl = build_ngraph_onnx('./..')
+    print('SUCCESS! Generated wheel: {}'.format(ngonnx_whl))
+
+    # Run a quick test
+    print('----- Install and load venv -----')
+    venv_dir = './ngonnx'
+    install_virtual_env(venv_dir)
+    load_venv(venv_dir)
+
+    print('----- Installing nGraph wheel -----')
+    install_wheel(pyngraph_whl)
+    print('----- Installing nGraph-ONNX wheel -----')
+    install_wheel(ngonnx_whl)
+
+    print('----- Test nGraph and nGraph-ONNX packages -----')
+    # download test data
+    if not os.path.exists('./resnet50'):
+        check_call(['wget',
+                    'https://s3.amazonaws.com/download.onnx/models/opset_8/resnet50.tar.gz'])
+        check_call(['tar', '-xzvf', 'resnet50.tar.gz'])
+
+    import onnx
+    import ngraph as ng
+    from ngraph_onnx.onnx_importer.backend import NgraphBackend
+    import numpy as np
+
+    ng.abs([[1, 2, 3], [4, 5, 6]])
+    # <Abs: 'Abs_1' ([2, 3])>
+
+    test_data = np.load('./resnet50/test_data_0.npz', encoding='bytes')
+    inputs = list(test_data['inputs'])
+    model = onnx.load('./resnet50/model.onnx')
+    prepared_model = NgraphBackend.prepare(model, 'CPU')
+    outputs = list(prepared_model.run(inputs))
+    ref_outputs = test_data['outputs']
+    np.testing.assert_equal(len(ref_outputs), len(outputs))
+    for i in range(len(outputs)):
+        np.testing.assert_equal(ref_outputs[i].dtype, outputs[i].dtype)
+        np.testing.assert_allclose(ref_outputs[i], outputs[i], rtol=1e-3, atol=1e-7)
+
+    print('SUCCESS!')
 
 if __name__ == '__main__':
     main()
