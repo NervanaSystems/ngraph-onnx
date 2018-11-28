1ba0b6352acaf4a579ab6145ec97bf3f997226e6
Adam Rogowiec <adam.rogowiec@intel.com>
2018-11-28 13:50:49 +0100

    Upgrade version to 0.9.0 (#255)
    * Upgrade version
    
    * Update package dependencies

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from distutils.core import setup
 from setuptools import find_packages
 
 setup(name='ngraph-onnx',
-      version='0.2.0',
+      version='0.9.0',
       description='ONNX support for ngraph',
       author='Intel',
       author_email='intelnervana@intel.com',
       url='http://www.intelnervana.com',
       license='License :: OSI Approved :: Apache Software License',
-      packages=find_packages(exclude=['tests']),
-      install_requires=['setuptools', 'numpy', 'onnx', 'cachetools'])
+      packages=find_packages(exclude=['tests', 'tests_core']),
+      install_requires=['cachetools', 'numpy', 'onnx', 'setuptools'])
