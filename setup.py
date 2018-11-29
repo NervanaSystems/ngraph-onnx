447971f7c9a8d9e0384ee59d2cf43c67717a3b9f
Adam Rogowiec <adam.rogowiec@intel.com>
2018-11-29 12:51:12 +0100

    Add license to generated package

--- setup.py
+++ setup.py
@@ -1,8 +1,19 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
-
 from setuptools import find_packages
+import os
+
+ONNX_IMPORT_SOURCE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+data_files = [
+    (
+        'licenses',
+        [
+            os.path.join(ONNX_IMPORT_SOURCE_DIR, 'LICENSE'),
+        ],
+    ),
+]
 
 setup(name='ngraph-onnx',
       version='0.10.0-rc.1',
@@ -12,4 +23,5 @@ setup(name='ngraph-onnx',
       url='http://www.intelnervana.com',
       license='License :: OSI Approved :: Apache Software License',
       packages=find_packages(exclude=['tests', 'tests_core']),
+      data_files=data_files,
       install_requires=['cachetools', 'numpy', 'onnx', 'setuptools'])
