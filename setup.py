9dec0b1e2fd0b914e38e8735c2a0b3859defbae1
Adam Rogowiec <adam.rogowiec@intel.com>
2018-11-29 14:02:25 +0100

    Refactor adding license file to package

--- setup.py
+++ setup.py
@@ -2,18 +2,6 @@
 
 from distutils.core import setup
 from setuptools import find_packages
-import os
-
-ONNX_IMPORT_SOURCE_DIR = os.path.abspath(os.path.dirname(__file__))
-
-data_files = [
-    (
-        'licenses',
-        [
-            os.path.join(ONNX_IMPORT_SOURCE_DIR, 'LICENSE'),
-        ],
-    ),
-]
 
 setup(name='ngraph-onnx',
       version='0.10.0-rc.1',
@@ -23,5 +11,5 @@ setup(name='ngraph-onnx',
       url='http://www.intelnervana.com',
       license='License :: OSI Approved :: Apache Software License',
       packages=find_packages(exclude=['tests', 'tests.*', 'tests_core', 'tests_core.*']),
-      data_files=data_files,
+      data_files=[('', ['LICENSE'])],
       install_requires=['cachetools', 'numpy', 'onnx', 'setuptools'])
