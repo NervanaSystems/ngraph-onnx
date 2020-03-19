e3ff4526711f31f3f31b163c6f7d50f4425a4589
Rafal Blaczkowski <rafal.blaczkowski@intel.com>
2020-03-19 14:55:59 +0000

    jjrevert changes

--- .ci/jenkins/prepare_environment.sh
+++ .ci/jenkins/prepare_environment.sh
@@ -39,7 +39,6 @@ function build_ngraph() {
     cd "${ngraph_directory}/ngraph"
     mkdir -p ./build
     cd ./build
-    ./bootstrap --system-curl
     cmake ${CMAKE_ARGS} ..  || return 1
     make -j $(lscpu --parse=CORE | grep -v '#' | sort | uniq | wc -l) || return 1
     make install || return 1
